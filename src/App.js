6782a7d986010ede0e245b8b7cb8e5406ee1170d
Touger Thao <tougerthao@gmail.com>
2022-04-05 11:10:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./components/Card";
+import lionGuardData from "../src/data";
+import { nanoid } from "nanoid";
 
-function App() {
+export default function App() {
+  const [data, setData] = React.useState(lionGuardData.data.cards);
+
+  const [cards, setCards] = React.useState([]);
+
+  React.useEffect(() => {
+    setCards(shuffleCards(generateAllCards(data)));
+  }, [data]);
+
+  function generateCard(dataElement) {
+    let card = {
+      id: nanoid(),
+      name: dataElement.name,
+      url: dataElement.url,
+      isFlipped: false,
+    };
+    return card;
+  }
+
+  function generateAllCards(data) {
+    let newCards = [];
+    shuffleCards(data);
+    for (let i = 0; i < data.length; i++) {
+      newCards.push(generateCard(data[i]));
+      newCards.push(generateCard(data[i]));
+    }
+    return newCards;
+  }
+
+  function swap(array, i, j) {
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  function shuffleCards(array) {
+    const length = array.length;
+    for (let i = length; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      const currIndex = i - 1;
+      swap(array, currIndex, randomIndex);
+    }
+
+    return array;
+  }
+
+  let cardsElement = cards.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        name={card.name}
+        isFlipped={card.isFlipped}
+        imageUrl={card.url}
+      />
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="app-title-container">
+        <h1 className="app-title">Memory Matching Game</h1>
+        <h4 className="app-description">
+          Select two cards with the same image to earn a point. The user with
+          the most points, wins!
+        </h4>
+      </div>
+      <div className="board-container">{cardsElement}</div>
+      <div className="points-and-btn-container">
+        <div className="points-container">
+          <h2 className="points-title">
+            # of Moves: <span className="points-number">10</span>
+          </h2>
+        </div>
+        <button className="new-game-btn">New Game</button>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export default {
+  success: true,
+  data: {
+    cards: [
+      {
+        id: "1",
+        name: "Simba",
+        url: "../images/simba.png",
+      },
+      {
+        id: "2",
+        name: "Kion",
+        url: "../images/kion.png",
+      },
+      {
+        id: "3",
+        name: "Beshte",
+        url: "../images/beshte.png",
+      },
+      {
+        id: "4",
+        name: "Bunga",
+        url: "../images/bunga.png",
+      },
+      /* {
+        id: "5",
+        name: "Fuli",
+        url: "../images/fuli.png",
+      },
+      {
+        id: "6",
+        name: "Janja",
+        url: "../images/janja.png",
+      },
+      {
+        id: "7",
+        name: "Johari",
+        url: "../images/johari.png",
+      },
+      {
+        id: "8",
+        name: "Kambuni",
+        url: "../images/kambuni.png",
+      },
+      {
+        id: "9",
+        name: "Kenge",
+        url: "../images/kenge.png",
+      },
+      {
+        id: "10",
+        name: "Kiara",
+        url: "../images/kiara.png",
+      },
+      {
+        id: "11",
+        name: "Makuu",
+        url: "../images/makuu.png",
+      },
+      {
+        id: "12",
+        name: "Rafiki",
+        url: "../images/rafiki.png",
+      },
+      {
+        id: "13",
+        name: "Rani",
+        url: "../images/rani.png",
+      },
+      {
+        id: "14",
+        name: "Scar",
+        url: "../images/scar.png",
+      },
+      {
+        id: "15",
+        name: "Vitani",
+        url: "../images/vitani.png",
+      }, */
+    ],
+  },
+};
