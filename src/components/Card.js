8c57362005760bb0b4f21dab9d82d7e1b79a8b98
Touger Thao <tougerthao@gmail.com>
2022-04-06 12:56:43 -0500

    added animation to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,30 +6,31 @@ import { Animated } from "react-animated-css";
 export default function Card(props) {
   return (
     <div className="card-container">
-      <Animated
+      {/* <Animated
         animationIn="fadeIn"
         animationOut="rollOut"
         isVisible={!props.matched}
       >
+        {props.isFlipped && <img
+          src={props.isFlipped ? `${props.imageUrl}` : lionGuardLogo}
+          alt={props.name}
+          className="card-img"
+          onClick={props.handleClick}
+        />} */}
+
+      {!props.matched && (
         <img
           src={props.isFlipped ? `${props.imageUrl}` : lionGuardLogo}
           alt={props.name}
           className="card-img"
           onClick={props.handleClick}
         />
-
-        {/*  {!props.matched && (
-          <img
-            src={props.isFlipped ? `${props.imageUrl}` : lionGuardLogo}
-            alt={props.name}
-            className="card-img"
-            onClick={props.handleClick}
-          />
-        )}
-        {props.matched && (
-          <img src={emptyImage} alt="empty" className="card-img-empty" />
-        )} */}
-      </Animated>
+      )}
+      {props.matched && (
+        <Animated animationOut="flipOutY" isVisible={!props.matched}>
+          <img src={props.imageUrl} alt="empty" className="card-img-empty" />
+        </Animated>
+      )}
     </div>
   );
 }
